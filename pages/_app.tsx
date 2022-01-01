f780f0219f38ad59588e747695badc8d0134d6c1
jeffreyshum <89279966+jeffreyshum@users.noreply.github.com>
2021-12-31 22:24:35 -0500

    Unified Import Style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
-import '../styles/codeblock.css'
-import '../styles/codetitle.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import "../styles/codeblock.css"
+import "../styles/codetitle.css"
+import type { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
