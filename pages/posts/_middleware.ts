5e9326263a8793587af9b751f027132eef8191a7
jeffreyshum <89279966+jeffreyshum@users.noreply.github.com>
2022-01-02 21:26:02 -0500

    Added Middleware Redirect

---
+++ pages/posts/_middleware.ts
@@ -0,0 +1,11 @@
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+    const { pathname } = req.nextUrl
+    
+    if (pathname == "/posts") {
+        return NextResponse.redirect("/courses")
+    }
+    
+    return NextResponse.next()
+}
