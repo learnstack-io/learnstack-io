125b979bdd03e9a017b48d9ee07a0bec38bc00d0
jeffreyshum <89279966+jeffreyshum@users.noreply.github.com>
2022-01-02 19:26:46 -0500

    Added Nav Links

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -10,8 +10,17 @@ const Nav: FC = () => {
     return (
         <nav className={styles.nav}>
             <Link href="/">
-                <img className={styles.logo} src="/icons/logo.svg" alt="Logo" />
+                <a><img className={styles.logo} src="/icons/logo.svg" alt="Logo" /></a>
             </Link>
+            <div className={styles.links}>
+                <Link href="/courses">
+                    <a><h1>Courses</h1></a>
+                </Link>
+                <Link href="/languages">
+                    <a><h1>Languages</h1></a>
+                </Link>
+            </div>
+            
         </nav>
     )
 }
